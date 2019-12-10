9eb22c7e9354ff2db8096a73ecd6d43b6caa565c
Yiyang Zhao <yiyang.zhao@columbia.edu>
2019-12-10 01:28:47 -0500

    Update cofi

--- Cofi/collaborative_filtering.py
+++ Cofi/collaborative_filtering.py
@@ -1,11 +1,15 @@
 import numpy as np
 import pandas as pd
+import math
 # import seaborn as sns
 # import matplotlib.pyplot as plt
 
 
 data = pd.read_csv('dummy1.csv')
-print(data)
+# print(data)
+myid = 16
+myrating = data[data['uid'] == myid]['init_rating'].tolist()
+print(myrating)
 
 data = data[['uid', 'eid', 'init_rating']]
 
@@ -15,7 +19,7 @@ reader = Reader()
 data = Dataset.load_from_df(data[['uid', 'eid', 'init_rating']], reader)
 
 from surprise.model_selection import train_test_split
-trainset, testset = train_test_split(data, test_size=0.25)
+trainset, testset = train_test_split(data, test_size=0.2)
 
 
 from surprise import SVD, accuracy
@@ -23,16 +27,29 @@ algo = SVD()
 output = algo.fit(trainset)
 predictions = algo.test(testset)
 
-
-
 from surprise import accuracy
 accuracy.rmse(predictions)
 
 
+
+rateList = []
 for i in range (1,11):
-    pred = algo.predict(uid = '14', iid = i)
-    score = pred.est
-    print(score)
+    pred = algo.predict(uid = myid, iid = i)
+    score = pred.est + math.sqrt(myrating[i-1])
+    rateList.append(score)
+copyList = rateList.copy()
+rateList.sort(reverse = True)
+
+print(copyList)
+print(rateList)
+
+
+rec = []
+for i in range (0,4):
+    rec.append(copyList.index(rateList[i])+1)
+print(rec)
+
+
 
 
 # https://blog.cambridgespark.com/tutorial-practical-introduction-to-recommender-systems-dbe22848392b
